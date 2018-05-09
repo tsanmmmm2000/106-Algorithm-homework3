f12e2a40b0e5dfc6fb24a24ab0128b22966ae19f
tsanming <tsanmmmm2000@hotmail.com>
2018-05-09 22:58:41 +0800

    add sort method

--- as.c
+++ as.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 #define MAX_ACTIVITY 1000
 
 struct activity {
-    int number[MAX_ACTIVITY];
-    int start[MAX_ACTIVITY];
-    int finish[MAX_ACTIVITY];
-    int value[MAX_ACTIVITY];
+    int number;
+    int start;
+    int finish;
+    int value;
 };
 
 struct activity_result {
@@ -20,26 +20,33 @@ int find_max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int reverse() {
-
+int compare(const void *a, const void *b) {
+    return *(int *) a - *(int *)b;
 }
 
-struct activity_result find_activity(struct activity acty, int n) {
-    // to do: sort by finish time
+int activity_compare(const void *a, const void *b) {
+    struct activity *acty_a = (struct activity *)a;
+    struct activity *acty_b = (struct activity *)b;
+    return (acty_a->finish - acty_b->finish);
+}
+
+struct activity_result find_activity(struct activity acty[], int n) {
     
+    qsort(acty, n, sizeof(struct activity), activity_compare);
+
     struct activity_result result;
     int max = 0;
     int max_index = 0;
 
     int temp[n];
     for (int i = 0; i < n; i++) {
-        temp[i] = acty.value[i];
+        temp[i] = acty[i].value;
     }
     
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            if (acty.start[i] >= acty.finish[j]) {
-                temp[i] = find_max(temp[i], temp[j] + acty.value[i]);
+            if (acty[i].start >= acty[j].finish) {
+                temp[i] = find_max(temp[i], temp[j] + acty[i].value);
                 if (temp[i] > max) {
                     max = temp[i];
                     max_index = i;
@@ -49,43 +56,28 @@ struct activity_result find_activity(struct activity acty, int n) {
     }
     result.max = max;    
 
-    int index = MAX_ACTIVITY - 1;
-    int size = 0;
+    int count = 0;
     for (int i = max_index; i >= 0 ; i--) {
         if (temp[i] == max) {
-            max -= acty.value[i];
-            result.number[index] = acty.number[i];
-            index--;
-            size++;
+            max -= acty[i].value;
+            result.number[count] = acty[i].number;
+            count++;
         }
     }
+
+    qsort(result.number, count, sizeof(int), compare);
     
-    result.size = size;
+    result.size = count;
     return result;
 }
-
 int main() {
-    struct activity acty;
-    acty.number[0] = 1;
-    acty.number[1] = 2;
-    acty.number[2] = 3;
-
-    acty.start[0] = 0;
-    acty.start[1] = 3;
-    acty.start[2] = 3;
-
-    acty.finish[0] = 3;
-    acty.finish[1] = 4;
-    acty.finish[2] = 5;
-
-    acty.value[0] = 4;
-    acty.value[1] = 1;
-    acty.value[2] = 2;    
-
-    struct activity_result result = find_activity(acty, 3);
+    struct activity acty[] = {
+        {1,8,21,19}, {2,10,15,12}, {3,4,9,18}, {4,20,30,25}, {5,0,5,15} 
+    };
+    struct activity_result result = find_activity(acty, 5);
     printf("%d\n", result.max);
-    for (int i = MAX_ACTIVITY - result.size; i < MAX_ACTIVITY; i++) {
+    for (int i = 0; i < result.size; i++) {
         printf("%d ", result.number[i]);
     }
-    printf("\n");
+    printf("\n"); 
 }
